4a5d67368f1b02c41b6d3ed0d49df62e32f1c3f9
airzero <29842352+airzero24@users.noreply.github.com>
2020-02-17 22:52:46 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple C# Port Scanning Tool
+
+This is a basic TCP single port scanner written in C#. Saw the code in a [ClearSky Security Report](https://www.clearskysec.com/wp-content/uploads/2020/02/ClearSky-Fox-Kitten-Campaign-v1.pdf) on the Fox Kitten campaign. Seemed like a useful tool for port validation before performing lateral movement.
---
+++ portscan/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace portscan
+{
+    class Program
+    {
+        private static bool Scan(string host, int port)
+        {
+            TcpClient tcpClient = new TcpClient { };
+            try
+            {
+                tcpClient.Connect(host, port);
+                tcpClient.Close();
+                return true;
+            }
+            catch
+            {
+                tcpClient.Close();
+                return false;
+            }
+        }
+
+        private static void Help()
+        {
+            string help = "\n\tUsage: portscan.exe <ip address> <port>" +
+                "\n\tExample: portscan.exe 192.168.1.2 22\n";
+            Console.WriteLine(help);
+        }
+
+        static void Main(string[] args)
+        {
+            if(args.Length != 2)
+            {
+                Console.WriteLine("[-] No arguments supplied");
+                Help();
+                Environment.Exit(1);
+            }
+            else
+            {
+                int port = 0;
+                try
+                {
+                    port = Int32.Parse(args[1]);
+                }
+                catch
+                {
+                    Console.WriteLine("[!] Port argument is not a valid port number");
+                    Help();
+                    Environment.Exit(1);
+                }
+                if(Scan(args[0], port))
+                {
+                    Console.WriteLine("[+] Port open");
+                }
+                else
+                {
+                    Console.WriteLine("[-] Port closed");
+                }
+            }
+        }
+    }
+}
